6062684485a94b71f2fcdd983939ed44dca37664
natnael abebaw <natnaell70a@gmail.com>
2023-12-02 13:01:21 -0500

    add booking model

---
+++ src/models/bookingModel.ts
@@ -0,0 +1,77 @@
+import mongoose, { Date } from "mongoose";
+import { Document } from "mongoose";
+import { differenceInDays } from "date-fns";
+
+export interface Booking extends Document {
+  guest: mongoose.Schema.Types.ObjectId;
+  cabin: mongoose.Schema.Types.ObjectId;
+  hotel: mongoose.Schema.Types.ObjectId;
+  checkInDate: Date;
+  checkOutDate: Date;
+  numNight: number;
+  cabinPrice: number;
+  extrasPrice: number;
+  totalPrice: number;
+  hasBreakfast: boolean;
+  paymentStatus: string;
+  status: string;
+}
+
+const bookingSchema = new mongoose.Schema<Booking>(
+  {
+    guest: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Guest",
+      required: true,
+    },
+    cabin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cabin",
+      required: true,
+    },
+    hotel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+    checkInDate: {
+      type: Date,
+      required: true,
+    },
+    checkOutDate: {
+      type: Date,
+      required: true,
+    },
+    cabinPrice: {
+      type: Number,
+      required: true,
+    },
+    extrasPrice: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["checkedin", "checkedout", "unconfirmed", "cancelled"],
+    },
+    hasBreakfast: {
+      type: Boolean,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "completed", "canceled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+bookingSchema.virtual("numNights").get(function (this: Booking) {
+  return differenceInDays(this.checkOutDate as any, this.checkInDate as any);
+});
+
+export const BookingModel = mongoose.model("Booking", bookingSchema);
