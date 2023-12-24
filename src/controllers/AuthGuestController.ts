82372910a15807ed63c9c2be9bc60c9571deb299
natnael abebaw <natnaell70a@gmail.com>
2023-12-24 20:08:41 +0300

    add routes to the auth

---
+++ src/controllers/AuthGuestController.ts
@@ -0,0 +1,51 @@
+import { controller } from "../decorators/controllerDecorator";
+import { protect } from "../decorators/authDecorators";
+import { get, post, patch, del } from "../decorators/routeHandlerDecorators";
+import { GuestModel } from "../models/guestModel";
+import { AuthController } from "./AuthController";
+
+@controller("/guests")
+export class AuthGuestController extends AuthController<typeof GuestModel> {
+  @post("/signup")
+  signupGuest() {
+    return this.signup(GuestModel);
+  }
+
+  @get("/login")
+  loginGuest() {
+    return this.login(GuestModel);
+  }
+
+  @get()
+  getAllGuests() {
+    return this.getAll(GuestModel);
+  }
+
+  @get("/forgetPassword")
+  guestForgetPassword() {
+    return this.forgetPassword(GuestModel);
+  }
+
+  @patch("/:userId/resetPassword/:token")
+  guestResetPassword() {
+    return this.resetPassword(GuestModel);
+  }
+
+  @protect()
+  @del("/deleteMe")
+  deleteMeForGuest() {
+    return this.deleteMe(GuestModel);
+  }
+
+  @protect()
+  @patch("/updateUserInfo")
+  updateguestInfo() {
+    return this.updateUserInfo(GuestModel);
+  }
+
+  @protect()
+  @patch("/updatePassword")
+  updateGuestPassword() {
+    return this.updatePassword(GuestModel);
+  }
+}
---
+++ src/controllers/AuthHotelController.ts
@@ -0,0 +1,53 @@
+import { controller } from "../decorators/controllerDecorator";
+import { get, post, patch, del } from "../decorators/routeHandlerDecorators";
+import { protect } from "../decorators/authDecorators";
+import { HotelAccountModel } from "../models/hotelAccount";
+import { AuthController } from "./AuthController";
+
+@controller("/hotelAccounts")
+export class AuthHotelController extends AuthController<
+  typeof HotelAccountModel
+> {
+  @post("/signup")
+  signupHotelAccount() {
+    return this.signup(HotelAccountModel);
+  }
+
+  @get("/login")
+  loginHotelAccount() {
+    return this.login(HotelAccountModel);
+  }
+
+  @get()
+  getAllHotelAccounts() {
+    return this.getAll(HotelAccountModel);
+  }
+
+  @get("/forgetPassword")
+  hotelAccountForgetPassword() {
+    return this.forgetPassword(HotelAccountModel);
+  }
+
+  @patch("/:userId/resetPassword/:token")
+  hotelAccountResetPassword() {
+    return this.resetPassword(HotelAccountModel);
+  }
+
+  @protect()
+  @del("/deleteMe")
+  deleteMeForHotelAccount() {
+    return this.deleteMe(HotelAccountModel);
+  }
+
+  @protect()
+  @patch("/updateUserInfo")
+  updateHotelAccountInfo() {
+    return this.updateUserInfo(HotelAccountModel);
+  }
+
+  @protect()
+  @patch("/updatePassword")
+  updateHotelAccountPassword() {
+    return this.updatePassword(HotelAccountModel);
+  }
+}
